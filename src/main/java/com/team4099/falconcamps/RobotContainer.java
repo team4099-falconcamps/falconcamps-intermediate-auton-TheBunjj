a09c7acd9d41d4811bbbbd073775f24b520f760c
TheBunjj <tenb7801@chsphantoms.org>
2020-07-16 17:31:51 +0000

    Part 1 Code

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -1,16 +1,22 @@
 package com.team4099.falconcamps;
 
 import com.team4099.falconcamps.commands.ShootCommand;
+import com.team4099.falconcamps.commands.ShooterIdleCommand;
 import com.team4099.falconcamps.subsystems.Drivetrain;
 import com.team4099.falconcamps.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
     public Drivetrain drivetrain = new Drivetrain();
     public Shooter shooter = new Shooter();
+    XboxController driver = new XboxController(0);
 
     public RobotContainer() {
-
+        new Trigger(driver :: getAButton).whenActive(new ShootCommand(shooter));
+        shooter.setDefaultCommand(new ShooterIdleCommand(shooter));
     }
 
     public Command getAutonomousCommand() {
